63de9bd61721807033d1f3e03ab99808e2df9303
ramkashyap21 <102165927+ramkashyap21@users.noreply.github.com>
2026-02-10 21:20:31 +0530

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -1,1 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase, ref, get, set, onValue } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBc_f9XBgUi3HMpFQU40fWJdzxscfV826s",
+  authDomain: "paisaeran-43a70.firebaseapp.com",
+  databaseURL: "https://paisaeran-43a70-default-rtdb.firebaseio.com",
+  projectId: "paisaeran-43a70"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) return;
+
+  const walletRef = ref(db, "users/" + user.uid + "/wallet");
+
+  // First time user → create wallet
+  get(walletRef).then((snap) => {
+    if (!snap.exists()) {
+      set(walletRef, {
+        balance: 0
+      });
+    }
+  });
+
+  // Live balance listener
+  onValue(walletRef, (snap) => {
+    if (snap.exists()) {
+      document.getElementById("balance").innerText =
+        "₹ " + snap.val().balance;
+    }
+  });
+});
